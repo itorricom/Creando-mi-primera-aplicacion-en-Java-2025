2ca2d773e51aaa313bfc9ee1ebc94e742ed4edc5
Isidoro Torrico M. <itorrico.m@gmail.com>
2025-04-10 17:10:48 -0400

    Lectura de datos

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -17,5 +17,21 @@ public class Decisiones {
             System.out.println("Pelicula no disponible para su plan actual");
         }
 
+        int dia = 3;
+        switch (dia) {
+            case 1:
+                System.out.println("El día 1 es lunes");
+                break;
+            case 2:
+                System.out.println("El día 2 es martes");
+                break;
+            case 3:
+                System.out.println("El día 3 es miércoles");
+                break;
+            // otros casos posibles...
+            default:
+                System.out.println("Día no válido");
+        }
+
     }
 }
